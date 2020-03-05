3dc6c46ba8d3a9ffa2ae9b144492d6dbdc5be09b
shrenikm <shrenik95@gmail.com>
2020-03-04 23:09:39 -0800

    Starting setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Neovim setup
+# ------------------------------------------------
+
+mkdir -p ~/.config/nvim
+ln -s init.vim ~/.config/nvim/init.vim
+
+# Tmux setup
+# ------------------------------------------------
+
