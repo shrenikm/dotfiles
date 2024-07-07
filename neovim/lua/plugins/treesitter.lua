1fb50a6f9b85193e115f9106e59d35c9b741ee5c
shrenikm <shrenik95@gmail.com>
2024-07-06 23:18:35 -0700

    okie

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "c", "cpp", "python", "bash", "lua" },
+      ensure_installed = { "c", "cpp", "python", "bash", "lua", "regex"},
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
