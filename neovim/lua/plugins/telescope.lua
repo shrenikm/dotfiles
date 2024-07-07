209984ae970662c9db9722d968049e582e7b1f06
shrenikm <shrenik95@gmail.com>
2024-07-06 20:42:09 -0700

    extension

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -1,9 +1,14 @@
 local keymap_opts = { noremap=true, unique=true, silent=true }
 
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    build = "make",
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  },
   opts = {
     
   },
@@ -17,16 +22,22 @@ return {
         }
       },
       pickers = {},
-      extensions = {},
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        }
+      },
     }
+    require("telescope").load_extension("ui-select")
+
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, keymap_opts)
-    vim.keymap.set('n', '<leader>fs', builtin.grep_string, keymap_opts)
-    vim.keymap.set('n', '<leader>fl', builtin.live_grep, keymap_opts)
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, keymap_opts)
-    vim.keymap.set('n', '<leader>fo', builtin.oldfiles, keymap_opts)
-    vim.keymap.set('n', '<leader>fc', builtin.command_history, keymap_opts)
-    vim.keymap.set('n', '<leader>fgs', builtin.git_status, keymap_opts)
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, keymap_opts)
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, keymap_opts)
+    vim.keymap.set("n", "<leader>fs", builtin.grep_string, keymap_opts)
+    vim.keymap.set("n", "<leader>fl", builtin.live_grep, keymap_opts)
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, keymap_opts)
+    vim.keymap.set("n", "<leader>fo", builtin.oldfiles, keymap_opts)
+    vim.keymap.set("n", "<leader>fc", builtin.command_history, keymap_opts)
+    vim.keymap.set("n", "<leader>fg", builtin.git_status, keymap_opts)
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, keymap_opts)
   end,
 }
