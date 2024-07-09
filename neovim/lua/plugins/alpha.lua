5d0433e9cc721ef2c2bc939c438e1a9f2a9a4dd3
shrenikm <shrenik95@gmail.com>
2024-07-08 23:40:51 -0700

    icons!

--- neovim/lua/plugins/alpha.lua
+++ neovim/lua/plugins/alpha.lua
@@ -25,12 +25,12 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
-      dashboard.button("r", "   Recent files", ":Telescope oldfiles<CR>"),
-      dashboard.button("m", "   Mason", ":Mason<CR>"),
-      dashboard.button("l", "   Lazy", ":Lazy<CR>"),
-      dashboard.button("q", "   Quit NVim", ":qa<CR>"),
+      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "󰈞   Find file", ":Telescope find_files<CR>"),
+      dashboard.button("r", "   Recent files", ":Telescope oldfiles<CR>"),
+      dashboard.button("m", "󱁤   Mason", ":Mason<CR>"),
+      dashboard.button("l", "   Lazy", ":Lazy<CR>"),
+      dashboard.button("q", "󰅙   Quit NVim", ":qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)
