2e346e073b1af510054fd6c0d57c4a35dc8904d2
shrenikm <shrenik95@gmail.com>
2024-07-09 01:58:07 -0700

    hunk preview

--- neovim/lua/plugins/gitsigns.lua
+++ neovim/lua/plugins/gitsigns.lua
@@ -1,9 +1,18 @@
+local KEYMAP_OPTS = { noremap = true, unique = false, silent = true }
+
 return {
 	"lewis6991/gitsigns.nvim",
 	name = "gitsigns",
 	config = function()
 		require("gitsigns").setup({
 			signs_staged_enable = true,
+
+      -- Keymappings. Using only the hunk preview for now (Used to preview the git signs status on the left)
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+        KEYMAP_OPTS["buffer"] = bufnr
+        vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, KEYMAP_OPTS)
+      end
 		})
 	end,
 }
