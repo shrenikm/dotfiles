566bf3a723a090f80f8533f067e02e6029e91998
shrenikm <shrenik95@gmail.com>
2024-07-13 17:32:10 -0700

    ensure installed for formatters

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -1,7 +1,29 @@
 -- Plugins for everything LSP
 
 -- Required language servers
-REQUIRED_LS = { "vimls", "cmake", "clangd", "pyright", "bashls", "lua_ls", "yamlls", "lemminx" }
+REQUIRED_LS = { "vimls", "cmake", "clangd", "pyright", "bashls", "lua_ls", "yamlls", "lemminx", "grammarly" }
+-- List of all required language servers and their options.
+LS_CONFIG = {
+	vimls = {},
+	cmake = {},
+	clangd = {},
+	pyright = {},
+	bashls = {},
+	lua_ls = {},
+	yamlls = {},
+	lemminx = {},
+	-- Running  grammarly on more files than just markdown
+	grammarly = {
+		filetypes = { "vim", "lua", "cmake", "c", "cpp", "python", "sh", "yaml", "xml", "markdown", "text", "toml" },
+	},
+}
+
+-- Extract the list of required language servers to be added to ensure_installed.
+
+REQUIRED_LS = {}
+for ls, _ in pairs(LS_CONFIG) do
+	table.insert(REQUIRED_LS, ls)
+end
 
 return {
 	{
@@ -87,7 +109,7 @@ return {
 			-- Cross dependency with completions. Not really avoidable.
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			for _, ls in ipairs(REQUIRED_LS) do
+			for ls, _ in pairs(LS_CONFIG) do
 				lspconfig[ls].setup({
 					capabilities = capabilities,
 				})
--- neovim/lua/plugins/none_ls.lua
+++ neovim/lua/plugins/none_ls.lua
@@ -1,42 +1,72 @@
 local KEYMAP_OPTS = { noremap = true, unique = false, silent = true }
 
 return {
-	"nvimtools/none-ls.nvim",
-	name = "none-ls",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
+	{
+		"nvimtools/none-ls.nvim",
+		name = "none-ls",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+          -- Code actions
+
+					-- Formatters (MAKE SURE THESE ARE INSTALLED THROUGH MASON)
+					-- For lua
+					null_ls.builtins.formatting.stylua,
+					-- For CMake
+					null_ls.builtins.formatting.cmake_format,
+					-- For C/C++
+					null_ls.builtins.formatting.clang_format,
+					-- For python
+					null_ls.builtins.formatting.black,
+					-- For python imports
+					null_ls.builtins.formatting.isort,
+					-- For codeblocks inside markdown
+					null_ls.builtins.formatting.cbfmt,
+					-- For markdown linting (Could optionally use mdformat)
+					null_ls.builtins.formatting.markdownlint,
+					-- For bash
+					null_ls.builtins.formatting.shfmt,
+					-- For yaml
+					null_ls.builtins.formatting.yamlfmt,
+					-- For xml
+					null_ls.builtins.formatting.xmllint,
+				},
+			})
+
+			-- Keybindings
+			vim.keymap.set("n", "<space>f", vim.lsp.buf.format, KEYMAP_OPTS)
+		end,
 	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				--null_ls.builtins.code_actions.refactoring,
-
-				-- Formatters (MAKE SURE THESE ARE INSTALLED THROUGH MASON)
-				-- For lua
-				null_ls.builtins.formatting.stylua,
-				-- For CMake
-				null_ls.builtins.formatting.cmake_format,
-				-- For C/C++
-				null_ls.builtins.formatting.clang_format,
-				-- For python
-				null_ls.builtins.formatting.black,
-				-- For python imports
-				null_ls.builtins.formatting.isort,
-				-- For codeblocks inside markdown
-				null_ls.builtins.formatting.cbfmt,
-				-- For markdown linting (Could optionally use mdformat)
-				null_ls.builtins.formatting.markdownlint,
-				-- For bash
-				null_ls.builtins.formatting.shfmt,
-				-- For yaml
-				null_ls.builtins.formatting.yamlfmt,
-				-- For xml
-				null_ls.builtins.formatting.xmllint,
-			},
-		})
-
-		-- Keybindings
-		vim.keymap.set("n", "<space>f", vim.lsp.buf.format, KEYMAP_OPTS)
-	end,
+  -- Primarily used to auto install Mason linters and formatters. mason-lspconfig cannot be used for this
+  -- as it can only ensure install LSPs.
+	{
+		"jay-babu/mason-null-ls.nvim",
+		name = "mason-null_ls",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"stylua",
+					"cmake_format",
+					"clang_format",
+					"black",
+					"isort",
+					"cbfmt",
+					"markdownlint",
+					"shfmt",
+					"yammlfmt",
+					"xmllint",
+				},
+				-- Automatically install masons tools based on selected sources in null-ls.
+				automatic_installation = true,
+			})
+		end,
+	},
 }
