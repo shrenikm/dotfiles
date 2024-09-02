f6b9a84306b78fb9bd224629cb93cdcbadeb4d71
shrenikm <shrenik95@gmail.com>
2024-09-01 22:48:36 -0700

    gitsigns mappings

--- neovim/lua/plugins/gitsigns.lua
+++ neovim/lua/plugins/gitsigns.lua
@@ -1,5 +1,6 @@
 local KEYMAP_OPTS = { noremap = true, unique = false, silent = true }
 
+
 return {
 	"lewis6991/gitsigns.nvim",
 	name = "gitsigns",
@@ -12,6 +13,12 @@ return {
 				local gitsigns = require("gitsigns")
 				KEYMAP_OPTS["buffer"] = bufnr
 				vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hb", gitsigns.toggle_current_line_blame, KEYMAP_OPTS)
+        vim.keymap.set("n", "<leader>hB", function() gitsigns.blame_line{full=true} end, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hD", function() gitsigns.diffthis('~') end, KEYMAP_OPTS)
 			end,
 		})
 	end,
