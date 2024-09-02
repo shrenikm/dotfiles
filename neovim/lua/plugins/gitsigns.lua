804ae5b5edfbff60042a001652fcf91b684f3ec5
shrenikm <shrenik95@gmail.com>
2024-09-01 22:53:18 -0700

    blame opts

--- neovim/lua/plugins/gitsigns.lua
+++ neovim/lua/plugins/gitsigns.lua
@@ -1,12 +1,16 @@
 local KEYMAP_OPTS = { noremap = true, unique = false, silent = true }
 
-
 return {
 	"lewis6991/gitsigns.nvim",
 	name = "gitsigns",
 	config = function()
 		require("gitsigns").setup({
 			signs_staged_enable = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 100,
+			},
 
 			-- Keymappings. Using only the hunk preview for now (Used to preview the git signs status on the left)
 			on_attach = function(bufnr)
@@ -16,9 +20,13 @@ return {
 				vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, KEYMAP_OPTS)
 				vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, KEYMAP_OPTS)
 				vim.keymap.set("n", "<leader>hb", gitsigns.toggle_current_line_blame, KEYMAP_OPTS)
-        vim.keymap.set("n", "<leader>hB", function() gitsigns.blame_line{full=true} end, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hB", function()
+					gitsigns.blame_line({ full = true })
+				end, KEYMAP_OPTS)
 				vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, KEYMAP_OPTS)
-				vim.keymap.set("n", "<leader>hD", function() gitsigns.diffthis('~') end, KEYMAP_OPTS)
+				vim.keymap.set("n", "<leader>hD", function()
+					gitsigns.diffthis("~")
+				end, KEYMAP_OPTS)
 			end,
 		})
 	end,
